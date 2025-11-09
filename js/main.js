b791784f50188fe667371471d32ce701842b9132
Sashimus <142751930+Sashimus@users.noreply.github.com>
2025-11-09 09:42:15 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const modalWindow = document.querySelector('.modal');
+const buttonModals = document.querySelectorAll('.modal-btn');
+const modalClose = document.querySelector('.modal-close');
+const body = document.querySelector('body');
+
+buttonModals.forEach((item) => {
+    item.addEventListener('click', () => {
+        modalWindow.style.display = 'flex';
+        body.classList.add('noscroll');
+    });
+});
+
+modalWindow.addEventListener('click', (e) => {
+    const isModal = e.target.closest('.modal-inner');
+
+    if (!isModal) {
+        modalWindow.style.display = 'none';
+        body.classList.remove('noscroll');
+    }
+});
+
+modalClose.addEventListener('click', () => {
+    modalWindow.style.display = 'none';
+    body.classList.remove('noscroll');
+});
